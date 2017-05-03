05a9332a1ef1de6650efae6d86aef6fc6179410a
norbiro000 <boomtingtong@hotmail.com>
2017-05-03 22:28:41 +0700

    try to use stream

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var http = require('http')
 var fs = require('fs')
 
 var server = http.createServer(function (req, res) {
-	fs.readFile(__dirname + '/data.txt', function (err, data) {
-		console.log(data)
-		res.end(data)
-	})
+	var stream = fs.createReadStream(__dirname + '/data.txt');
+	stream.pipe(res)
 })
 
 server.listen(3000)
