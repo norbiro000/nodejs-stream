11e7c66259125a47e028a511593d3ebec8192e34
norbiro000 <boomtingtong@hotmail.com>
2017-05-03 22:45:52 +0700

    learning about Readable stream

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http')
 var fs = require('fs')
+var oppressor = require('oppressor')
 
 var server = http.createServer(function (req, res) {
 	var stream = fs.createReadStream(__dirname + '/data.txt');
-	stream.pipe(res)
+	stream.pipe(oppressor(req)).pipe(res)
+	
 })
 
 server.listen(3000)
